fff5cfef4ab2b40fb5d7798484f9d1f2ce4d1868
Federico Sioli <fsioli@udesa.edu.ar>
2020-10-26 16:46:57 -0300

    .

--- js/main.js
+++ js/main.js
@@ -579,22 +579,22 @@
         "time": {
             "Days": {
                 "text": "Days",
-                "color": "#0038E3",
+                "color": "#002047",
                 "show": true
             },
             "Hours": {
                 "text": "Hours",
-                "color": "#0038E3",
+                "color": "#002047",
                 "show": true
             },
             "Minutes": {
                 "text": "Minutes",
-                "color": "#0038E3",
+                "color": "#002047",
                 "show": true
             },
             "Seconds": {
                 "text": "Seconds",
-                "color": "#0038E3",
+                "color": "#002047",
                 "show": true
             }
         }
